f731cacdd0121078b0b35777b69bfd207cc3ee82
FNSY <fadynabil96@gmail.com>
2020-03-25 13:28:59 +0200

    First Flutter Application. Trying appBar.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text('Fady\'s First App'),
+      ),
+
+    );
+  }
+
+}
