c1d80c31a4ac8401eadb55404dffb56289f71d5c
FNSY <fadynabil96@gmail.com>
2020-03-25 16:04:17 +0200

    Separate the build method to several methods.

--- lib/main.dart
+++ lib/main.dart
@@ -14,32 +14,41 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.amber,
         title: Text(
           'Fady\'s First App',
-          style: TextStyle(fontFamily: 'ShadowsIntoLightTwo', fontSize: 30),
+          style: myAppTextStyle(),
         ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Text(
-              'Let\'s develope Apps!',
-              style: TextStyle(
-                  color: Colors.amber, fontSize: 30, fontFamily: 'BadScript'),
-            ),
-            color: Colors.black54,
-            margin: EdgeInsets.symmetric(horizontal: 60, vertical: 30),
-            padding: EdgeInsets.fromLTRB(30, 40, 30, 40),
-          ),
-          Image.asset('images/innuva_logo.jpg')
-        ],
+        children: <Widget>[textBoxContainer(), innuvaLogoImageAsset()],
       ),
-      floatingActionButton: IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.email),
-        iconSize: 70,
-        color: Colors.lightGreen,
-      ),
+      floatingActionButton: emailButton(),
     );
   }
+}
+
+Image innuvaLogoImageAsset() {
+  return Image.asset('images/innuva_logo.jpg');
+}
+
+Container textBoxContainer() {
+  return Container(
+    child: Text('Let\'s develope Apps!', style: myAppTextStyle()),
+    color: Colors.black54,
+    margin: EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+    padding: EdgeInsets.fromLTRB(30, 40, 30, 40),
+  );
+}
+
+TextStyle myAppTextStyle() {
+  return TextStyle(color: Colors.black, fontSize: 30, fontFamily: 'BadScript');
+}
+
+IconButton emailButton() {
+  return IconButton(
+    onPressed: () {},
+    icon: Icon(Icons.email),
+    iconSize: 70,
+    color: Colors.lightBlueAccent,
+  );
 }
