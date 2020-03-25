3e7baab335697efa56683632ace56d73063e030b
FNSY <fadynabil96@gmail.com>
2020-03-25 14:26:37 +0200

    Add container, used padding and margins.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-));
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
   @override
@@ -12,8 +14,16 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.amber,
         title: Text('Fady\'s First App'),
       ),
-
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Text('Let\'s develope Apps!'),
+            color: Colors.black54,
+            margin: EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+            padding: EdgeInsets.fromLTRB(30, 40, 50, 60),
+          )
+        ],
+      ),
     );
   }
-
 }
